e0ee306a5afba5304cab0baf39eaf247723687b5
Silverhawk <sondra.silverhawk@hush.com>
2018-09-26 10:42:35 -0700

    Improve model organization and squash migrations

--- game/admin.py
+++ game/admin.py
@@ -1,6 +1,8 @@
 from django.contrib import admin
-from .models import Character, Item, Monster
+from .models import Character, ModelItem, RealItem, Monster
 
 admin.site.register(Character)
-admin.site.register(Item)
+admin.site.register(ModelItem)
+
+admin.site.register(RealItem)
 admin.site.register(Monster)
--- game/models.py
+++ game/models.py
@@ -1,28 +1,51 @@
 from django.contrib.postgres.fields import ArrayField
 from django.db import models
+from django.core.validators import MaxValueValidator, MinValueValidator
+
+# Common Properties
+class Descriptive(models.Model):
+    name = models.CharField(max_length=200)
+    description = models.TextField(blank=True, default='')
+
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        abstract=True
+
+class Sentient(Descriptive):
+    ac = models.IntegerField(default=1, validators=[MaxValueValidator(1000), MinValueValidator(1)])
+    max_hp = models.IntegerField(default=1, validators=[MaxValueValidator(10000), MinValueValidator(0)])
+    current_hp = models.IntegerField(default=1, validators=[MaxValueValidator(10000), MinValueValidator(0)])
+
+    class Meta:
+        abstract=True
 
 # Models in alphebetical order
 
 # Character
-class Character(models.Model):
+class Character(Sentient):
     player = models.ForeignKey('auth.User', on_delete=models.CASCADE, blank=True, null=True)
-    name = models.CharField(max_length=200)
+    
+    class Meta:
+        db_table = 'character'
 
-    def __str__(self):
-        return self.name
-# Item
-class Item(models.Model):
-    name = models.CharField(max_length=200)
+# Items
+# For conceptual items that could exist
+class ModelItem(Descriptive):
+    class Meta:
+        db_table = 'model_item'
+
+# For actual instances of a conceptual item
+class RealItem(ModelItem):
     holder = models.ForeignKey(Character, on_delete=models.CASCADE, blank=True, null=True)
-    description = models.TextField(blank=True, default='')
 
-    def __str__(self):
-        return self.name
+    class Meta:
+        db_table = 'real_item'
 
 # Monster
-class Monster(models.Model):
-    name = models.CharField(max_length=200)
-    description = models.TextField(blank=True, default='')
+class Monster(Sentient):
+    
+    class Meta:
+        db_table = 'monster'
 
-    def __str__(self):
-        return self.name
