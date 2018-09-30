48aa0667d0625f84a96a82e9c89dc404d3139d6b
Silverhawk <sondra.silverhawk@hush.com>
2018-09-29 21:32:26 -0700

    Add views for individual items

--- game/urls.py
+++ game/urls.py
@@ -4,5 +4,6 @@ from . import views
 urlpatterns = [
     path('', views.home, name='home'),
     path('items', views.model_item_list, name='items'),
+    path('item/<int:pk>/', views.item_detail, name='item_detail'),
     path('rules', views.rules, name='rules'),
 ]
--- game/views.py
+++ game/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from .models import ModelItem
 
 def home(request):
@@ -7,6 +7,10 @@ def home(request):
 def model_item_list(request):
     items = ModelItem.objects.all()
     return render(request, 'game/item_list.html', {'items': items})
+
+def item_detail(request, pk):
+    item = get_object_or_404(ModelItem, pk=pk)
+    return render(request, 'game/item_detail.html', {'item': item})
 
 def rules(request):
     return render(request, 'game/rules.html', {})
